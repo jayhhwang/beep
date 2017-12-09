f78d01e2882b3d0634e0248a85e9b7ff9b263cb8
jayhhwang <34406195+jayhhwang@users.noreply.github.com>
2017-12-09 15:42:52 -0800

    Update beep.py

--- beep.py
+++ beep.py
@@ -1,12 +1,15 @@
 import os
 import time
 
-t0 = time.time()
-
 def alert():
 	return os.system('spd-say -t female3 "your code has finished"')
 
-def duration():
+def start():
+	global t0 
+	t0 = time.time()
+	return
+
+def end():
 	t1 = time.time()
 	total = t1-t0
 	return print("Your code took %f seconds" % total)
