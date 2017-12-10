20c700d3731953f68ab95c7b43815fb2c8e64849
jayhhwang <34406195+jayhhwang@users.noreply.github.com>
2017-12-09 18:26:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name='beep',
 	packages = ['beep'],
-	version='0.1',
+	version='1.0',
 	description='A simple alert and timer',
 	url='https://github.com/jayhhwang/beep',
 	long_description=open('README.rst', 'rt').read(),
