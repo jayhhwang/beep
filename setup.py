d3e4b123a14bec54d40d032d6557f0b802d7a523
jayhhwang <34406195+jayhhwang@users.noreply.github.com>
2017-12-09 17:34:06 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='beep',
+	packages = ['beep'],
+	version='0.1',
+	description='A simple alert and timer',
+	url='https://github.com/jayhhwang/beep',
+	long_description=open('README.rst', 'rt').read(),
+	classifiers=[],
+	author='Jay Hwang',
+	author_email='thebluebirdspeaks@gmail.com'
+)
